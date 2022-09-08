86fcec00a4bd732db6452067581e2a88ee26b731
Thuong <ngdinhthuong@gmail.com>
2022-09-08 16:59:53 +0700

    lab_05.3

--- lab_05/src/components/MainComponent.js
+++ lab_05/src/components/MainComponent.js
@@ -5,6 +5,11 @@ import Header from './Header';
 import Footer from './Footer';
 import Home from './Home';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './Contact';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotion';
+import { LEADERS } from '../shared/leaders';
+
 
 class main extends Component {
   constructor(props) {
@@ -17,14 +22,19 @@ class main extends Component {
     return (
       <div className="container-fluid">        
         <Header />
-        <Home/>
+        <Home 
+              dish={this.state.dishes?.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions?.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders?.filter((leader) => leader.featured)[0]}
+          />
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <Footer />
 
         <Switch>
           <Route path='/home' component={Home} />
+          <Route exact path='/contactus' component={Contact} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-          {/* <Redirect to="/home" /> */}
+          <Redirect to="/home" />
         </Switch>
       </div>
     )
