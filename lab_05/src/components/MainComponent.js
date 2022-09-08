a586897c02477839b41650a2750614c14098d48b
Thuong <ngdinhthuong@gmail.com>
2022-09-08 16:37:37 +0700

    lab_05.2

--- lab_05/src/components/MainComponent.js
+++ lab_05/src/components/MainComponent.js
@@ -3,6 +3,8 @@ import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
 import Header from './Header';
 import Footer from './Footer';
+import Home from './Home';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class main extends Component {
   constructor(props) {
@@ -12,14 +14,21 @@ class main extends Component {
     };
   }
   render() {
-   return(
-    <div className="container-fluid">
-    <Header/>
-    <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>
-    <Footer />
-    </div>
-   )
-}
+    return (
+      <div className="container-fluid">        
+        <Header />
+        <Home/>
+        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <Footer />
+
+        <Switch>
+          <Route path='/home' component={Home} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          {/* <Redirect to="/home" /> */}
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default main;
